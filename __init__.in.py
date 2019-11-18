be3efdd4642fe3b7d6439ea5162836dac022453d
Godó Bence <52776215+godesz@users.noreply.github.com>
2019-11-18 10:52:16 +0100

    Update __init__.in.py

--- __init__.in.py
+++ __init__.in.py
@@ -1,51 +1,3 @@
-###############################
-#  This file is part of PyLaDa.
-#
-#  Copyright (C) 2013 National Renewable Energy Lab
-#
-#  PyLaDa is a high throughput computational platform for Physics. It aims to
-#  make it easier to submit large numbers of jobs on supercomputers. It
-#  provides a python interface to physical input, such as crystal structures,
-#  as well as to a number of DFT (VASP, CRYSTAL) and atomic potential programs.
-#  It is able to organise and launch computational jobs on PBS and SLURM.
-#
-#  PyLaDa is free software: you can redistribute it and/or modify it under the
-#  terms of the GNU General Public License as published by the Free Software
-#  Foundation, either version 3 of the License, or (at your option) any later
-#  version.
-#
-#  PyLaDa is distributed in the hope that it will be useful, but WITHOUT ANY
-#  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-#  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
-#  details.
-#
-#  You should have received a copy of the GNU General Public License along with
-#  PyLaDa.  If not, see <http://www.gnu.org/licenses/>.
-###############################
-
-""" Root of all pylada python packages and modules.
-
-    Configuration variables exist here. However, they are added within separate
-    files. Which files will depend upon the user.
-
-       - Files located in the config sub-directory where pylada is installed
-       - Files located in one of the directories specified by
-         :envvar:`PYLADA_CONFIG_DIR`
-       - In the user configuration file ~/.pylada
-
-    The files are read in that order. Within a given directory, files are read
-    alphabetically. Later files can override previous files. Finally, all and
-    any variable which are declared within these files will end up at the root
-    of :py:mod:`pylada`. Be nice, don't pollute yourself.
-
-    .. envvar:: PYLADA_CONFIG_DIR
-
-       Environment variable specifying the path(s) to the configuration
-       directories.
-
-    For which variables can be set (and, if relevant, which file) see
-    pylada.config.
-"""
 __docformat__ = "restructuredtext en"
 __all__ = [
     "load_ipython_extension", "unload_ipython_extension",
@@ -137,15 +89,10 @@ try:
 except ImportError:
     pass
 else:
-    # Make sure this function is not run by pytest, to avoid infinit
     # recursion
     @mark.skipif(True, "Helper function to aggregate tests")
     def test(**kwargs):
-        """ Run all pylada python tests
-
-            Does not tests that require external programs such as vasp. Those should be run via
-            ctest.
-        """
+        
         from os.path import dirname
         from pytest import main
         return main(dirname(__file__), **kwargs)
